1c7ba8d003732ef8e4ae38518ba29e4649cc4ab3
Luis <50057252+luisperez1050@users.noreply.github.com>
2022-05-30 20:20:04 -0400

    Update validation.js

--- server/api/validation.js
+++ server/api/validation.js
@@ -3,6 +3,10 @@ import { HtmlValidate } from 'html-validate-es';
 import { formatterFactory } from 'html-validate';
 export default defineEventHandler( async (req, res) => {
     res.setHeader("Access-Control-Allow-Origin", "*");
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept"
+    );
     const body = await useBody(req);
     const validateHtml = new HtmlValidate();
     const validate = validateHtml.validateString(body);
